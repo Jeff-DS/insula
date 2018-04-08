aacd6efaf077bb8c5725e3781bd99f2981f4db4c
Jeff Spingeld <jeff.spingeld@gmail.com>
2018-04-08 15:24:57 -0700

    Add page for landlord/tenant users to choose role to log in as

---
+++ src/MakeRolePage.js
@@ -0,0 +1,11 @@
+import React from 'react-js';
+
+const RolePicker = () => (
+  <div id="RolePicker">
+    <h1>Welcome to Insula</h1>
+    <button>Landlord Dashboard</button>
+    <button>Tenant Dashboard</button>
+  </div>
+)
+
+export default RolePicker;
