5b4403184d56a7a5a7a9891348ca5ba65472452b
jeff_ds <jeff.spingeld+cloud9@gmail.com>
2018-06-19 01:42:23 +0000

    Add user schema

---
+++ apiServer/models/user.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// This "pre hook" lets us arrange for a function to be called
+// before a new document is saved to the db
+userSchema.pre("save", async function(next){
+    try {
+        if (!this.isModified("password")){
+            return next();
+        }
+        let hashedPassword =  await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch(err){
+        return next(err);
+    }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword, next){
+    try{
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        console.log(".comparePasswrod: isMatch = ", isMatch);
+        return isMatch;
+    } catch(err){
+        console.log("error in .comparePassword");
+        return next(err);
+    }
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
